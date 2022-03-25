d7121aace555200bc6ca62f087405c2ed2291c6f
Rafsan <rafsan.quayes@gmail.com>
2022-03-25 21:03:38 +0600

    Made the server work on route '/'

--- QuestionService/index.js
+++ QuestionService/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 7777;
 
+app.get("/", (req, res)=>{
+    res.send("Question Viewer");
+    
+})
+
 app.listen(port, ()=>{
     console.log(`Server Running at port ${port}`);
 })

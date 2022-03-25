06f1bd03ac128882f8f289083d9eda8fccbe36fc
Rafsan <rafsan.quayes@gmail.com>
2022-03-25 21:46:53 +0600

    Can now get questions from databaseCan now get questions from databaseCan now get questions from databaseCan now get questions from databaseCan now get questions from databaseCan now get questions from databaseCan now get questions from databaseCan now get questions from databaseCan now get questions from database

--- QuestionService/index.js
+++ QuestionService/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 7777;
 
 const mongoose = require('mongoose');
-const mongoURL = 'mongodb://admin:admin@authexpress-shard-00-00.l8kxn.mongodb.net:27017,authexpress-shard-00-01.l8kxn.mongodb.net:27017,authexpress-shard-00-02.l8kxn.mongodb.net:27017/QuestionService?ssl=true&replicaSet=atlas-260l3u-shard-0&authSource=admin&retryWrites=true&w=majority';
+const mongoURL = 'mongodb://admin:admin@authexpress-shard-00-00.l8kxn.mongodb.net:27017,authexpress-shard-00-01.l8kxn.mongodb.net:27017,authexpress-shard-00-02.l8kxn.mongodb.net:27017/QuestionServiceDatabase?ssl=true&replicaSet=atlas-260l3u-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 //Connect to Database
 mongoose
@@ -18,9 +18,39 @@ mongoose
     console.log(error);
   })
 
+// Create the model for questions
+const QuestionModel = mongoose.model('Questions', new mongoose.Schema({
+        _id: {
+            type: String,
+            required: true
+        },
+        Question: {
+            type: String,
+            required: true
+        },
+        Answer: {
+            type: String,
+            required: true
+        },
+        Tag: {
+            type: String,
+            required: true
+        },
+        Options: []
+    })
+);
 
 app.get("/", (req, res)=>{
-    res.send("Question Viewer");
+    QuestionModel.find()
+        .then((data)=>{
+            res.send(data);
+        })
+        .catch((error)=>{
+            console.log(error);
+            res.send(error);
+        })
+
+    
 });
 
 app.listen(port, ()=>{
